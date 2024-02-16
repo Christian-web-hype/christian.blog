541f61ac89c946fa8b41f95070b48cbccb66aab0
Christian-web-hype <christiancasano6@gmail.com>
2024-02-16 22:57:29 +0100

    bozza pagina portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,3 +1,176 @@
 <x-layout>
+    <header class="container-fluid header-portfolio">
+        <div class="row">
+            
+            <div class="col-6 my-5 d-flex justify-content-center align-items-center">
+                <div class="a"></div>
+            </div>
+            
+            <div class="col-6 my-5 d-flex flex-column justify-content-center align-items-center">
+                <h1 class="tit-portfolio text-center">Esplora il Mio <span class="grassetto">Mondo Creativo</span></h1>
+                <h2 class="tit-portfolio text-center">Scopri la mia collezione di template</h2>
+                <p class="text-center p-portfolio">Progettati per elevare la tua presenza online ogni design riflette la mia passione per l'eccellenza. Sfoglia i template qui sotto e immergiti nel mondo dello sviluppo web e del design.</p>
+            </div>
+            
+        </div>
+    </header>
     
+    <div class="scuro">
+        <section class="container-fluid">
+            <div class="row">
+                
+                <div class="col-12 justify-content-center align-items-center my-5">
+                    <h2 class="tit-portfolio text-center">Template Creati con <span class="grassetto">Passione</span> e <span class="grassetto">Precisione</span></h2>
+                </div>
+                
+                <div class="col-12 d-flex justify-content-evenly align-items-center mt-5">
+                    <!-- modal 1 -->
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop1">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                    {{-- modal 2 --}}
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                    {{-- modal 3 --}}
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop3">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                </div>
+                
+                <div class="col-12 d-flex justify-content-evenly align-items-center my-5">
+                    
+                    {{-- modal 4 --}}
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop4">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                    <!-- modal 5 -->
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop5">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                    {{-- modal 6 --}}
+                    <button type="button" class="btn btn-modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop6">
+                        <div class="a-p"></div>
+                    </button>
+                    
+                </div>
+                
+            </div>
+        </section>
+    </div>
 </x-layout>
+
+
+
+
+
+
+<!-- Modal 1 -->
+<div class="modal fade" id="staticBackdrop1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+                
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                
+            </div>
+                
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Modal 2 -->
+<div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                bom
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- Modal 3 -->
+<div class="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                bom
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- Modal 4 -->
+<div class="modal fade" id="staticBackdrop4" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                bom
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<!-- Modal 5 -->
+<div class="modal fade" id="staticBackdrop5" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal 6 -->
+<div class="modal fade" id="staticBackdrop6" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body modal-dialog modal-dialog-scrollable">
+                bom
+            </div>
+
+        </div>
+    </div>
+</div>
