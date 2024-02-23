ec21352b32a3deaf0b61bb1c806ab78b4fe33269
Christian-web-hype <christiancasano6@gmail.com>
2024-02-23 14:32:24 +0100

    proviamo firebase

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount = ServiceAccount::fromJsonFile(config('firebase.credentials_path'));
+
+$firebase = (new Factory)
+    ->withServiceAccount($serviceAccount)
+    ->withDatabaseUri(config('firebase.database_url'))
+    ->create();
+
+$database = $firebase->getDatabase();
 
 class PublicController extends Controller
 {
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,5 +21,29 @@
 
     <x-footer/>
 
+
+    <script type="module">
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyARVQVLuaksgUYofDb7auM52om9yropbqs",
+          authDomain: "christian-casano-blog.firebaseapp.com",
+          projectId: "christian-casano-blog",
+          storageBucket: "christian-casano-blog.appspot.com",
+          messagingSenderId: "562262821143",
+          appId: "1:562262821143:web:ad86cfc4864fea9aed8f52",
+          measurementId: "G-NE0HT1Y3B7"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+      </script>
 </body>
 </html>
